e9c1b40dbd596af85ddee16523cd7732490ce934
Pedro Baumann <ondoheer@gmail.com>
2019-11-30 12:17:22 -0500

    se corrige gameover

--- scenes/gameOver.js
+++ scenes/gameOver.js
@@ -1,5 +1,6 @@
 export const gameOver = (ctx, text = "Game Over", x, y) => {
     ctx.font = "50px serif";
+    ctx.fillStyle = "#f00";
     ctx.fillText(text, x, y);
 };
 //# sourceMappingURL=gameOver.js.map
